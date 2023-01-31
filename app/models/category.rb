8cfa4fe37c87e4fc9b1df92dedf30d3b8b6d3257
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-01-31 16:11:53 +0100

    add kaminari gem

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
 
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true
+    paginates_per 2
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -4,4 +4,5 @@ class Operation < ApplicationRecord
   validates :amount, numericality: true
   validates :odate, presence: true
   validates :description, presence: true
+  paginates_per 2
 end 
