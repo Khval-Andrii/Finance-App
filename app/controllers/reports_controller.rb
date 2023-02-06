da37ae9169c526d2c6b111065cce7b6c936504a7
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-02-06 13:15:32 +0100

    refactor report form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,23 +3,24 @@ class ReportsController < ApplicationController
   end
 
   def report_by_category
-    categories_array = Operation.all.map { |oper| 
+    operations_by_categories = Operation.all.map { |oper| 
       [oper.amount.to_f, oper.category_id] if oper.odate >= params[:start_date] && oper.odate <= params[:end_date]
       }
+    
+    categories_array = Category.all.map { |c| [c.id, c.name]}
+    
     @amount = categories_array.map { |c| c[0] }
-    @category = categories_array.map { |c| c[1] }
-    @start_date = params[:start_date]
-    @end_date = params[:end_date]
+    
+    @category_by_id = categories_array.map { |c| c[1] }
   end
 
   def report_by_dates
     operations_array = Operation.all.map { |oper| 
       [oper.amount.to_f, oper.odate.to_s] if oper.odate >= params[:start_date] && oper.odate <= params[:end_date]
       }
-    @amount = operations_array.map { |amount| amount[0] }
+    
+    @amount = operations_array.map { |amount| amount[0] } 
     @dates = operations_array.map { |date| date[1] }
-    @start_date = params[:start_date]
-    @end_date = params[:end_date]
   end
 
   def action_report
