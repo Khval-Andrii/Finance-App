bdaf4ab659da1b680bb1ff0971a94b7faf15110f
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-02-09 12:35:05 +0100

    add validates for Types attr

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -4,5 +4,6 @@ class Operation < ApplicationRecord
   validates :amount, numericality: true
   validates :odate, presence: true
   validates :description, presence: true
+  validates :types, numericality: { only_integer: true, in: 1..2 }
   paginates_per 3
 end 
