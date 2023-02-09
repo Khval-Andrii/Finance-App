4e2d083d5f5f0a35884255daefc46d37ab0638bd
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-02-09 17:00:38 +0100

    refactor report by dates Action

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,25 +4,24 @@ class ReportsController < ApplicationController
 
   def report_by_category
     oper_arr = Operation
-      .order(:odate)
       .where('odate BETWEEN ? AND ?', params[:start_date], params[:end_date])
       .group(:category_id)
       .sum(:amount)
     @amount = oper_arr.map { |a| a[1] }
-   # puts(@amount)
     @category_id = Category.find(oper_arr.map { |a| a[0] }).map { |c| [c.name] }
-    #puts(@category_id)
   end
 
   def report_by_dates
-    operations_array = Operation.all.map { |oper| 
-      [oper.amount.to_f, oper.odate.to_date.to_s] if oper.odate >= params[:start_date] && oper.odate <= params[:end_date]
-      }
+    oper_arr = Operation
+      .where('odate BETWEEN ? AND ?', params[:start_date], params[:end_date])
+      .order(:odate)
+      .group(:odate)
+      .sum(:amount)
     
-    @amount = operations_array.map { |amount| amount[0] } 
-    @dates = operations_array.map { |date| date[1] }
+    @amount = oper_arr.map { |amount| amount[1].to_f }
+    @dates = oper_arr.map { |date| date[0].to_date.to_s }
   end
-
+  
   def action_report
     if params[:btn_graph]
       redirect_to({
@@ -39,5 +38,4 @@ class ReportsController < ApplicationController
         })
     end
   end
-
 end
