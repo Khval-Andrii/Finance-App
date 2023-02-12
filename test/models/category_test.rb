2e80482c17ffc8b35b17bc719ebdaf0d53f9dc95
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-02-12 16:12:34 +0100

    add model test for category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "return true if everything is ok" do
+    new_category = Category.new(name: "new_name", description: "new_description")
+    assert(new_category.valid?)
+  end
+
+  test "return false if anything is failed" do
+    new_category = Category.new(description: "new_description")
+    assert_not(new_category.valid?)
+  end
+
+  test "return false if anything is failed" do
+    new_category = Category.new(name: "new_name")
+    assert_not(new_category.valid?)
+  end
+
+  test "save and gathering" do
+    new_category = Category.new(name: "new_name", description: "new_description")
+    new_category.save()
+    new_category_1 = Category.find_by(name: "new_name")
+    assert_equal("new_description", new_category_1.description)
+  end
+
+  test "return true if names is unique" do
+    new_category = Category.create(name: "MyName_3", description: "MyDescription_3")
+    assert(new_category.valid?)
+  end
+
+  test "return false if names is not unique" do
+    new_category = Category.create(name: "MyName_2", description: "MyDescription_2")
+    assert_not(new_category.valid?)
+  end
+
+
 end
