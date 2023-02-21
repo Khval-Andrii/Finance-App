2ab26bb7b248cd47ddd8b4e6bcc826a3e457645e
Khval-Andrii <andrey.khval.kr@gmail.com>
2023-02-21 22:59:00 +0100

    change operation pagination

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -5,7 +5,7 @@ class Operation < ApplicationRecord
   validates :odate, presence: true
   validates :description, presence: true
   validates :types, numericality: { only_integer: true, in: 1..2 }
-  paginates_per 4
+  paginates_per 3
 
   def self.model_report_by_category(start_date, end_date, type_operations)
     Operation
