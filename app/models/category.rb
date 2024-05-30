8fc9fc8f1eaf99d2a87d0eea3407bbd196a07abc
Khval-Andrii <andrey.khval.kr@gmail.com>
2024-05-30 12:22:44 +0200

    establishing associations between models and User

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
+# class Category
 class Category < ApplicationRecord
-    has_many :operations, dependent: :destroy
+  has_many :operations, dependent: :destroy
+  belongs_to :user
 
-    validates :name, presence: true, uniqueness: true
-    validates :description, presence: true
-    paginates_per 4
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :description, presence: true
+
+  paginates_per 4
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
+# class User
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :categories, dependent: :destroy
+  has_many :operations, dependent: :destroy
+
+  validates :email, uniqueness: true
 end
