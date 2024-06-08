b841b8b746bc1dd3ac383303b256281e54034a68
Khval-Andrii <andrey.khval.kr@gmail.com>
2024-06-08 17:12:08 +0200

    add methods to get user`s Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,28 @@
 # class CategoriesController 
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!, except: %i[index]
   before_action :set_category, only: %i[show edit update destroy]
-  before_action :authenticate_user!, only: %i[show new edit create update destroy]
+  before_action :categories_types_set, only: %i[new edit create update]
 
   def index
-    @categories = Category.page(params[:page])
+    if user_signed_in?
+      session[:category_index_page] = params[:page] || 1
+      @categories = Category.categories_of_user(current_user.id).page(session[:category_index_page])
+    else
+      @categories = Category.categories_of_user(0).page(params[:page])
+    end
   end
 
   def show; end
 
   def new
-    @category = Category.new
+    @category = current_user.categories.new
   end
 
   def edit; end
 
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.new(category_params)
 
     if @category.save
       redirect_to category_url(@category), notice: 'Category was successfully created.'
@@ -41,10 +47,16 @@ class CategoriesController < ApplicationController
   private
 
   def set_category
-    @category = Category.find(params[:id])
+    @category = current_user.categories.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to categories_path, notice: 'Record not found'
   end
 
   def category_params
-    params.require(:category).permit(:name, :description)
+    params.require(:category).permit(:name, :description, :types)
+  end
+
+  def categories_types_set
+    @types = Category.categories_types
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,22 @@
-# class Category
+# FrozenStringLiteral
 class Category < ApplicationRecord
   has_many :operations, dependent: :destroy
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: { scope: :user_id }
+  enum types: { expense: 1, income: 2 }
+
+  validates :name, presence: true, uniqueness: { scope: :user_id, message: 'This category has already exist.' }
   validates :description, presence: true
+  validates :user_id, presence: true
+  validates :types, inclusion: { in: types.keys }
 
   paginates_per 4
+
+  def self.categories_of_user(user)
+    Category.where(user_id: user).order(:name)
+  end
+
+  def self.categories_types
+    Category.types.keys.map { |type| [type.humanize, type] }
+  end
 end
